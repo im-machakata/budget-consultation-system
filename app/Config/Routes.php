1e2249027bf6b60cc8ccb53006a8c0678fffeb94
Isaac Machakata <isaac@caasi.co.zw>
2024-05-13 16:31:52 +0200

    chore: add migration route for deployement script

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Controllers\UsersController;
+use App\Controllers\MigrationController;
 use CodeIgniter\Router\RouteCollection;
 
 /** @var RouteCollection $routes */
@@ -11,3 +12,6 @@ $routes->post('/login', [UsersController::class, 'createSession']);
 
 // todo: user must be logged in to logout
 $routes->get('/logout', [UsersController::class, 'logout']);
+
+// migration url 
+$routes->get('/_/', [MigrationController::class, 'index']);
---
+++ app/Controllers/MigrationController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\BaseController;
+use CodeIgniter\HTTP\ResponseInterface;
+
+class MigrationController extends BaseController
+{
+    public function index()
+    {
+        $update = $this->request->getGet('update') ?? false;
+
+        if (!$update) {
+            // rollback db
+            command('migrate:rollback');
+        }
+
+        // run new migrations
+        command('migrate');
+
+        // say hello world or something
+        return 'It is done!';
+    }
+}
