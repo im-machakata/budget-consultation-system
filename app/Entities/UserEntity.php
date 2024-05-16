77206a80c23cef5d10ff4e1ed89643da4a9f6e7d
Isaac Machakata <isaac@caasi.co.zw>
2024-05-16 05:08:35 +0200

    feat: get fullname from user entity

--- app/Entities/UserEntity.php
+++ app/Entities/UserEntity.php
@@ -9,4 +9,10 @@ class UserEntity extends Entity
     protected $datamap = [];
     protected $dates   = ['created_at', 'updated_at', 'deleted_at'];
     protected $casts   = [];
+
+    public function getFullName()
+    {
+        if (!$this->id) return null;
+        return sprintf('%s %s', $this->firstname, $this->lastname);
+    }
 }
