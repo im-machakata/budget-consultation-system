584ba23437f54fe2983a4000c4969eddc825be48
Isaac Machakata <isaac@caasi.co.zw>
2024-05-17 15:17:25 +0200

    feat: show error when request is blocked

--- app/ThirdParty/CommentsModerator.php
+++ app/ThirdParty/CommentsModerator.php
@@ -17,7 +17,7 @@ class CommentsModerator
     private static function init()
     {
         self::$client = Gemini::client(env('app.gemini.apiKey') ?? '');
-        self::$filter = "Analyse the message, if it's in Shona, translate it. When that's done, check if it is safe for a public school platform, not abusive, and does not contain any hate speech. Return the response as a JSON with a safe boolean key, translation, error_message (if any) and context message. Your reply must be plain text.\nThe message: ";
+        self::$filter = "Analyse the message, if it's in Shona, translate it. When that's done, check if it is safe for a public school platform, not abusive, does not have dirty words, and does not contain any hate speech, it must be children friendly. Return the response as a JSON with a safe boolean key, translation, error_message (if any) and context message. Your reply must be plain text.\nThe message: ";
     }
 
     public static function check(string $comment, int $count = 1): object
@@ -44,16 +44,19 @@ class CommentsModerator
             ->startChat(history: [
                 Content::parse(part: self::$filter),
             ]);
-        $result = $chat->sendMessage(self::$filter . '\n\r' . $comment)->text();
         try {
+            $result = $chat->sendMessage(self::$filter . '\n\r' . $comment)->text();
             $response = json_decode($result);
         } catch (\Exception $e) {
-            $response = json_decode(str_replace(['```json', '```'], '', $result));
+            if ($response) $response = json_decode(str_replace(['```json', '```'], '', $result));
         }
 
+        // if result is empty, then it's probably because Gemini blocked the prompt
+        if (!$result) return json_decode(json_encode(['error_message' => 'Our systems do not process such comments, please rethink and comment again.', 'safe' => false]));
+
+        // sometimes a timeout causes it to return null, so try again
         if (!$response) return self::check($comment, $count + 1);
 
-        log_message('info', json_encode($response));
         return $response;
     }
 }
